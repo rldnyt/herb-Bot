4392cfe5033b2642d989bfa385c9957bbbd6abcd
기우 <47372263+rldnyt@users.noreply.github.com>
2021-02-20 13:00:19 +0900

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class BotSettings:
     TEST_mode: 테스트모드의 실행 여부
     developer: 개발자 권한을 적용할 유저의 ID
     """
-    TOKEN = "ODEyMTU2Mzk2NDA5MzIzNTQy.YC8plg.gDCjH5tCDYqZn4XQ2PdNnAYXoIQ"
-    TESTBOT_TOKEN = "ODEyMTcyNTQ2MTgyNjExMDA1.YC84oA.PhcGakeuvixG38vSmY1PvrDYAsM"
-    prefix = "="
-    TEST_mode = True
-    developer = [371959898814152717, 473771823415296020]
-    updeveloper = [371959898814152717]
+    TOKEN = ""
+    TESTBOT_TOKEN = ""
+    prefix = ""
+    TEST_mode = False
+    developer = []
+    updeveloper = []
